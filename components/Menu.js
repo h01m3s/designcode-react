a0cd70700b7fab8bbc2601fb6cdb4f70a567f143
h01m3s <lin426797@gmail.com>
2019-02-24 23:49:04 -0500

    Menu added.

--- components/Menu.js
+++ components/Menu.js
@@ -1,21 +1,69 @@
 import React from "react";
+import { Animated, TouchableOpacity, Dimensions } from "react-native";
 import styled from "styled-components";
+import { Icon } from "expo";
+
+const screenHeight = Dimensions.get("window").height;
 
 class Menu extends React.Component {
+  state = {
+    top: new Animated.Value(screenHeight)
+  };
+
+  componentDidMount() {
+    Animated.spring(this.state.top, {
+      toValue: 0
+    }).start();
+  }
+
+  toggleMenu = () => {
+    Animated.spring(this.state.top, {
+      toValue: screenHeight
+    }).start();
+  };
+
   render() {
     return (
-      <Container>
+      <AnimatedContainer style={{ top: this.state.top }}>
         <Cover />
+        <TouchableOpacity onPress={this.toggleMenu}>
+          <CloseView>
+            <Icon.Ionicons name="ios-close" size={44} color="#546bfb" />
+          </CloseView>
+        </TouchableOpacity>
         <Content />
-      </Container>
+      </AnimatedContainer>
     );
   }
 }
 
 export default Menu;
 
-const Container = styled.View``;
+const CloseView = styled.View`
+  width: 44px;
+  height: 44px;
+  border-radius: 22px;
+  background: white;
+  justify-content: center;
+  align-items: center;
+`;
 
-const Cover = styled.View``;
+const Container = styled.View`
+  position: absolute;
+  background: white;
+  width: 100%;
+  height: 100%;
+  z-index: 100;
+`;
 
-const Content = styled.View``;
+const AnimatedContainer = Animated.createAnimatedComponent(Container);
+
+const Cover = styled.View`
+  height: 142px;
+  background: black;
+`;
+
+const Content = styled.View`
+  height: ${screenHeight};
+  background: #f0f3f5;
+`;
