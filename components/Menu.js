1d4b74d2ca4d3e66a7f25c661eea0e762207c1be
h01m3s <lin426797@gmail.com>
2019-02-25 21:27:23 -0500

    States and Animations

--- components/Menu.js
+++ components/Menu.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Animated, TouchableOpacity, Dimensions } from "react-native";
 import styled from "styled-components";
 import { Icon } from "expo";
+import MenuItem from "./MenuItem";
 
 const screenHeight = Dimensions.get("window").height;
 
@@ -25,19 +26,59 @@ class Menu extends React.Component {
   render() {
     return (
       <AnimatedContainer style={{ top: this.state.top }}>
-        <Cover />
-        <TouchableOpacity onPress={this.toggleMenu}>
+        <Cover>
+          <Image source={require("../assets/background2.jpg")} />
+          <Title>h01m3s</Title>
+          <Subtitle>Designer at Design+Code</Subtitle>
+        </Cover>
+        <TouchableOpacity
+          onPress={this.toggleMenu}
+          style={{
+            position: "absolute",
+            top: 120,
+            left: "50%",
+            marginLeft: -22,
+            zIndex: 1
+          }}
+        >
           <CloseView>
             <Icon.Ionicons name="ios-close" size={44} color="#546bfb" />
           </CloseView>
         </TouchableOpacity>
-        <Content />
+        <Content>
+          {items.map((item, index) => (
+            <MenuItem
+              key={index}
+              icon={item.icon}
+              title={item.title}
+              text={item.text}
+            />
+          ))}
+        </Content>
       </AnimatedContainer>
     );
   }
 }
 
 export default Menu;
+
+const Image = styled.Image`
+  position: absolute;
+  width: 100%;
+  height: 100%;
+`;
+
+const Title = styled.Text`
+  color: white;
+  font-size: 24px;
+  font-weight: 600;
+`;
+
+const Subtitle = styled.Text`
+  font-size: 13px;
+  color: rgba(255, 255, 255, 0.5);
+  margin-top: 8px;
+`;
 
 const CloseView = styled.View`
   width: 44px;
@@ -46,6 +87,7 @@ const CloseView = styled.View`
   background: white;
   justify-content: center;
   align-items: center;
+  box-shadow: 0 5px 10px rgba(0, 0, 0, 0.15);
 `;
 
 const Container = styled.View`
@@ -61,9 +103,35 @@ const AnimatedContainer = Animated.createAnimatedComponent(Container);
 const Cover = styled.View`
   height: 142px;
   background: black;
+  justify-content: center;
+  align-items: center;
 `;
 
 const Content = styled.View`
   height: ${screenHeight};
   background: #f0f3f5;
+  padding: 50px;
 `;
+
+const items = [
+  {
+    icon: "ios-settings",
+    title: "Account",
+    text: "settings"
+  },
+  {
+    icon: "ios-card",
+    title: "Billing",
+    text: "payments"
+  },
+  {
+    icon: "ios-compass",
+    title: "Learn React",
+    text: "start course"
+  },
+  {
+    icon: "ios-exit",
+    title: "Log out",
+    text: "see you soon!"
+  }
+];
